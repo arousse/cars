6727f0838600a6c9db33bdfa0f51576b6176f034
arousmalek <malek230197@gmail.com>
2017-09-15 20:06:12 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
 
-    $("#btn-Cars").click(function () {
+    $("#btn-cars").click(function () {
+        console.log("Cars");
         $("#myCarousel1").show();
         $("#myCarousel2").hide();
     });
 
 
     $("#btn-motors").click(function () {
+        console.log("motors");
         $("#myCarousel2").show();
         $("#myCarousel1").hide();
     });
