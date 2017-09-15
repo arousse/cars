9024d0bb920cfc91dc3d3a7e7017685b21e71cf1
Malek Boubakri <malekbouba@gmail.com>
2017-09-15 19:47:09 +0100

    addition

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
 
-    $("#btn-cars").click(function () {
-        console.log("Cars");
-        $("#myCarousel1").show();
-        $("#myCarousel2").hide();
-    });
-
-
-    $("#btn-motors").click(function () {
-        console.log("motors");
-        $("#myCarousel2").show();
-        $("#myCarousel1").hide();
-    });
+  $("#btn-cars").click(function () {
+    console.log("Cars");
+    $("#myCarousel1").show();
+    $("#myCarousel2").hide();
+    $("#panel-cars").show();
+    $("#panel-motors").hide();
+  });
+  
+  $("#btn-motors").click(function () {
+    console.log("motors");
+    $("#myCarousel2").show();
+    $("#myCarousel1").hide();
+    $("#panel-motors").show();
+    $("#panel-cars").hide();
+  });
 
 })
